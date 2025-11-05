3b056490edc572bdc69b06eba3996e4bbd72fe64
bhabanaagrawal1 <bhabanaagrawal1111@gmail.com>
2025-11-05 11:04:20 +0530

    Connected backend to Clever Cloud DB

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,6 @@
 import mysql from "mysql2";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const db = mysql.createConnection({
@@ -7,12 +8,15 @@ const db = mysql.createConnection({
   user: process.env.MYSQL_ADDON_USER,
   password: process.env.MYSQL_ADDON_PASSWORD,
   database: process.env.MYSQL_ADDON_DB,
-  port: process.env.MYSQL_ADDON_PORT
+  port: process.env.MYSQL_ADDON_PORT || 3306,
 });
 
 db.connect((err) => {
-  if (err) console.error("❌ Error connecting to MySQL:", err);
-  else console.log("✅ Connected to Clever Cloud MySQL Database");
+  if (err) {
+    console.error("❌ Error connecting to MySQL:", err);
+  } else {
+    console.log("✅ Connected to Clever Cloud MySQL Database");
+  }
 });
 
 export default db;
